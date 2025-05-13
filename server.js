1c0495d9ea2aaaa0a50508ef3ea3baf8ec223c2d
ivanshifman <ivanshifman1300@gmail.com>
2025-05-13 16:05:25 -0300

    finish front

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+// import path from "path";
 import portfolioRoutes from "./routes/portFolioRoute.js";
 
 dotenv.config();
@@ -9,8 +10,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+// app.use(express.static(path.join(__dirname, "./client/build")));
 app.use("/api/v1/portfolio", portfolioRoutes);
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 const PORT = process.env.PORT || 8080;
 
